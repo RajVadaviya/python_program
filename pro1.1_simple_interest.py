42efa3345caa13aeb96660a5733a572531a8a870
Students <student@mefgi.com>
2026-01-20 11:50:20 +0530

    Simple interest program

---
+++ pro1.1_simple_interest.py
@@ -0,0 +1,9 @@
+# Enter number and perform arithmetic operation
+
+p = int(input("Enter amount : "))
+r = float(input("Enter rate of intererest : "))
+n = float(input("Enter number of years : "))
+
+result = (p*r*n)/100
+
+print(result)
